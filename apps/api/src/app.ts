3ff5f598da16f4817abc0979d1777cb76fc59dba
ruanvalente <ruanopensuselinux@gmail.com>
2026-01-22 09:38:59 -0300

    chore: fix cors

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,7 +1,10 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+
 import { taskRoutes } from "./modules/tasks/task.routes";
 import { healthResponse } from "./shared/http";
 
 export const app = new Elysia({ name: "bunstack-api" })
   .get("/health", () => healthResponse)
-  .use(taskRoutes);
+  .use(taskRoutes)
+  .use(cors());
